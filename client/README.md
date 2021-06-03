9ba163ee76d37b6a27fd8495a8b3bddb2c48da6c
Seunghyo <seunghyoku@gmail.com>
2021-06-04 02:16:36 +0900

    [FE] init: FE 작업 시작하기 - styled-components, styled-reset 설치

---
+++ client/README.md
@@ -0,0 +1,9 @@
+### MBTI WITH LOL Client Page
+
+#### Skills
+
+- Javascript
+- React.js
+- Styled-components
+- Context API
+- Storybook
---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <>
+      <Reset />
+      <div className="App">
+        <header className="App-header">
+          <p>Home</p>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
