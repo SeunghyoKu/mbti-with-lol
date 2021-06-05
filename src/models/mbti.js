9a3ac5aa71fe539d5ab9ca9a33c7e27facfdb080
Seunghyo <seunghyoku@gmail.com>
2021-06-05 15:26:51 +0900

    feat: mbti 스키마 수정 - 이미지를 넣기 위한 스키마 수정 작업

--- src/models/mbti.js
+++ src/models/mbti.js
@@ -2,24 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Mbti = new Schema({
-  mbti: {
-    type: String,
-  },
-  comment: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  recommendation: {
-    type: [String],
-  },
-  lck: {
-    type: [String],
-  },
-  friend: {
-    type: [String],
-  },
+  mbti: String,
+  image: String,
+  comment: String,
+  description: String,
+  recommendation: [{ name: String, image: String }],
+  lck: [String],
+  friend: [{ mbti: String, image: String }],
 });
 
 module.exports = mongoose.model("mbti", Mbti);
