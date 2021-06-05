67de2c7a946110a0fa4e10093bfcce158e9031ea
Seunghyo <seunghyoku@gmail.com>
2021-06-06 00:54:32 +0900

    feat: 결과창 챔피언 밑에 이름 뜨게 하기

--- client/src/components/Champion/index.jsx
+++ client/src/components/Champion/index.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const Champion = ({ source }) => {
-  return <StyledChampion src={source}></StyledChampion>;
+import P from "components/PGothic";
+
+const Champion = ({ source, name }) => {
+  return (
+    <StyledDiv>
+      <StyledChampion src={source}></StyledChampion>
+      <P description={name} margin="5"></P>
+    </StyledDiv>
+  );
 };
 
 export default Champion;
 
-const StyledChampion = styled.img`
+const StyledDiv = styled.div`
   margin: 10px;
   width: 20%;
+`;
+
+const StyledChampion = styled.img`
+  width: 100%;
   border: 4px solid #0c8599;
   border-radius: 50%;
 `;
